33b39f8ae6def403192f740be0fe93f755a9fcb2
arbertrary <arbertrary@github>
2022-04-11 22:20:32 +0200

    update css js

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -8,8 +8,13 @@ const today = new Date()
 for (const day of days) {
     const id = day.id.replace("day", "");
 
-
     if (today.getDate() === parseInt(id)) {
+        doors = day.getElementsByClassName("door-closed");
+        
+        day.addEventListener("click", event => {
+            // console.log("test")
+            doors[0].setAttribute("class", "door-open")
+        });
         console.log(id);
     }
 }
